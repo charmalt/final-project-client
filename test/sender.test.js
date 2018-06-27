326a11f0060a1d7ac98696bcfcac3de5b1d6a692
John Newman <newmanj903@gmail.com>
2018-06-27 16:26:12 +0100

    Create Response Processor method

--- test/sender.test.js
+++ test/sender.test.js
@@ -1,8 +1,11 @@
 const Sender = require('../lib/sender')
+const sinon = require('sinon')
 
 describe('Sender', function () {
   let connectionMock = {
-    on: jest.fn()
+    on: jest.fn(),
+    write: jest.fn(),
+    destroy: jest.fn()
   }
   let message = {
     MAIL_FROM: 'john@john.com',
@@ -10,15 +13,33 @@ describe('Sender', function () {
     DATA: 'Hi Igor'
   }
   describe('send method', function () {
-    it('should call ', function () {
+    it('should call connection.on ', function () {
       Sender.send(message, connectionMock)
       expect(connectionMock.on).toHaveBeenCalledWith('connect', expect.any(Function))
     })
+    xit('should call Sender._handshake', function () {
+      let handshakeSpy = jest.spyOn(Sender, '_handshake')
+      Sender.send(message, connectionMock)
+      expect(handshakeSpy).toHaveBeenCalled()
+    })
   })
   describe('_handshake', function () {
-    it('should call .write method', function () {
-
+    it('should call ehlo method', function () {
+      let ehloSpy = jest.spyOn(Sender, 'ehloMethod')
+      Sender._handshake(message, connectionMock)
+      expect(ehloSpy).toHaveBeenCalled()
     })
   })
 
+  describe('ehloMethod', function () {
+    it('call connection.write', function () {
+      Sender.ehloMethod(connectionMock)
+      expect(connectionMock.write).toHaveBeenCalledWith('EHLO')
+    })
+
+    it('call connection.on', function () {
+      Sender.ehloMethod(connectionMock)
+      expect(connectionMock.on).toHaveBeenCalledWith('data', expect.any(Function) )
+    })
+  })
 })

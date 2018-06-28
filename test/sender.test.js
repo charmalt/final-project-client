98a9432dbbf4062e547e8c98e243a6c9ebb76c9a
benfurber <ben@furber-consultancy.com>
2018-06-28 17:50:23 +0100

    Added _mailFromMethod to Sender

--- test/sender.test.js
+++ test/sender.test.js
@@ -3,7 +3,6 @@
 const Sender = require('../lib/sender')
 jest.mock('net')
 
-
 describe('Sender', function () {
   let Socket = require('net').Socket
   let message = {
@@ -13,7 +12,9 @@ describe('Sender', function () {
   }
   let connection = new Socket()
   let sender = new Sender(message, connection)
-  
+
+  let connectionSpy = jest.spyOn(connection, 'write')
+
   describe('send', () => {
     it('should call handshake', () => {
       let handshakeSpy = jest.spyOn(sender, '_handshake')
@@ -21,18 +22,24 @@ describe('Sender', function () {
       expect(handshakeSpy).toHaveBeenCalled()
     })
   })
-  
+
   describe('_handshake', () => {
     xit('should call the first function of sender.functionOrder', () => {
       expect(sender._handshake).to
     })
   })
-  
+
   describe('_ehloMethod', () => {
     it('should write EHLO to the socket', function () {
-      let connectionSpy = jest.spyOn(connection, 'write')
       sender._ehloMethod()
       expect(connectionSpy).toHaveBeenCalledWith('EHLO')
+    })
+  })
+
+  describe('_mailFromMethod', () => {
+    it("should write 'MAIL FROM:' plus the given email address", () => {
+      sender._mailFromMethod()
+      expect(connectionSpy).toHaveBeenCalledWith(`MAIL FROM: ${sender.message.MAIL_FROM}`)
     })
   })
 })

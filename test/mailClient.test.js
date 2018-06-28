f59bab03674a335a6f0b766e7908c9a4df56f80b
benfurber <ben@furber-consultancy.com>
2018-06-28 11:58:19 +0100

    Add port and host to MailClient

--- test/mailClient.test.js
+++ test/mailClient.test.js
@@ -4,12 +4,22 @@ jest.mock('net')
 
 describe('MailClient', () => {
   let mailClient
+  let clientPort = 1337
+  let clientHost = '127.0.0.1'
 
   beforeEach(() => {
-    mailClient = new MailClient()
+    mailClient = new MailClient(clientPort, clientHost)
   })
 
-  it('to have a connection property that is set to null', () => {
+  it('has a connection property that is set to null', () => {
     expect(mailClient.connection).toEqual(null)
+  })
+
+  it('has a port property', () => {
+    expect(mailClient.port).toEqual(clientPort)
+  })
+
+  it('has a host property', () => {
+    expect(mailClient.host).toEqual(clientHost)
   })
 })

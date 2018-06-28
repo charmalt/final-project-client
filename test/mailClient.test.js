0090f312ad9c02c87ef68d0dd1ac225603d6bf6d
benfurber <ben@furber-consultancy.com>
2018-06-28 11:54:35 +0100

    Clear files for refactor and add connection property to MailClient

--- test/mailClient.test.js
+++ test/mailClient.test.js
@@ -1,49 +1,15 @@
 /* globals describe, it, expect, jest, beforeEach */
 const MailClient = require('../lib/MailClient')
 jest.mock('net')
-jest.mock('../lib/sender')
 
 describe('MailClient', () => {
-  let Socket = require('net').Socket
-  let Sender = require('../lib/sender')
-  let spyNewConnection
-  let spySocket
   let mailClient
-  const PORT = 1337
-  const HOST = '127.0.0.1'
+
   beforeEach(() => {
     mailClient = new MailClient()
-    spyNewConnection = jest.spyOn(mailClient, 'newConnection')
-    Sender.send = () => {}
   })
 
-  it('should create a new socket on newConnection()', () => {
-    mailClient.newConnection()
-    expect(spyNewConnection).toHaveBeenCalled()
-  })
-
-  it('should connect to a server on connect()', () => {
-    mailClient.newConnection()
-    spySocket = jest.spyOn(mailClient.connection, 'connect')
-    mailClient.connect(PORT, HOST)
-    expect(spySocket).toHaveBeenCalled()
-  })
-
-  it('sets sendMode to on', () => {
-    mailClient.sendModeOn()
-    expect(mailClient.sendMode).toBeTruthy()
-  })
-
-  it('sets sendMode to off', () => {
-    mailClient.sendModeOff()
-    expect(mailClient.sendMode).toBeFalsy()
-  })
-
-  it('sends the message if sendMode is on', () => {
-    mailClient.newConnection()
-    mailClient.sendModeOn()
-    let Senderspy = jest.spyOn(Sender, 'send')
-    mailClient.connect(PORT, HOST)
-    expect(Senderspy).toHaveBeenCalledWith(mailClient.message, mailClient.connection.connect(PORT, HOST))
+  it('to have a connection property that is set to null', () => {
+    expect(mailClient.connection).toEqual(null)
   })
 })

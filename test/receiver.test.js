86aa0ccd23d6868c43ab5322cacde013082a58c4
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-29 13:47:38 +0100

    Add _responseProcessor to the receiver

--- test/receiver.test.js
+++ test/receiver.test.js
@@ -33,4 +33,49 @@ describe('Receiver', () => {
     })
   })
 
+  describe('checkResponse', () => {
+    xit('calls _responseProcessor', () => {
+      let response = 'anything'
+      let expectedResponse = '250'
+      let processorSpy = jest.spyOn(receiver, '_responseProcessor')
+      receiver.checkResponse(response)
+      expect(processorSpy).toHaveBeenCalledWith(response, expectedResponse)
+    })
+  })
+
+  describe('_responseProcessor', () => {
+    it('should console.log the response received', function () {
+      console.log = jest.fn()
+      let actualResponse = 5
+      let expectedResponse = '5'
+      receiver._responseProcessor(actualResponse, expectedResponse)
+      expect(console.log).toHaveBeenCalledWith(actualResponse.toString())
+    })
+
+    it('should call connection.end if response differs from expected response', function () {
+      let connectionSpy = jest.spyOn(connection, 'end')
+      let actualResponse = 5
+      let expectedResponse = '6'
+      receiver._responseProcessor(actualResponse, expectedResponse)
+      expect(connectionSpy).toHaveBeenCalled()
+    })
+
+    it('should remove the first member of sender._functionOrder', function () {
+      let actualResponse = 5
+      let expectedResponse = '5'
+      let firstFunction = receiver._functionOrder[0]
+      receiver._responseProcessor(actualResponse, expectedResponse)
+      expect(receiver._functionOrder[0]).not.toEqual(firstFunction)
+    })
+
+    it('should call the _handshake method', function () {
+      let actualResponse = 5
+      let expectedResponse = '5'
+      let handshakeSpy = jest.spyOn(receiver, '_handshake')
+      handshakeSpy.mockClear()
+      receiver._responseProcessor(actualResponse, expectedResponse)
+      expect(handshakeSpy).toHaveBeenCalled()
+    })
+  })
+
 })

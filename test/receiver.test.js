e9d13e466473a51de64fcb459b2daeb5638895cf
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-29 13:42:40 +0100

    Add receive, _handshake, _helloMethod method to Receiver

--- test/receiver.test.js
+++ test/receiver.test.js
@@ -4,7 +4,33 @@ const Receiver = require('../lib/receiver')
 jest.mock('net')
 
 describe('Receiver', () => {
-  it('works', () => {
-    expect(2).toEqual(2)
+  let Socket = require('net').Socket
+  let messageJSON = {
+    "messageObject": {
+      "mailFrom": "john@john.com",
+      "rcptTo": "igor@john.com",
+      "messageBody": "Hi, Igor\n"
+    }
+  }
+  let connection = new Socket()
+  let receiver
+  beforeEach(() => {
+    receiver = new Receiver(connection)
   })
+  describe('receive method', () => {
+    it('calls _handshake', () => {
+      let handshakeSpy = jest.spyOn(receiver, '_handshake')
+      receiver.receive()
+      expect(handshakeSpy).toHaveBeenCalled()
+    })
+  })
+
+  describe('_helloMethod', () => {
+    let connectionSpy = jest.spyOn(connection, 'write')
+    it('should write "Hello" to the connection', function () {
+      receiver._helloMethod()
+      expect(connectionSpy).toHaveBeenCalledWith('Hello')
+    })
+  })
+
 })

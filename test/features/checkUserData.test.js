c21362cc29a5ddd6157a90a4574d6cfba52ba604
benfurber <ben@furber-consultancy.com>
2018-07-01 19:53:10 +0100

    Pass CheckUserData.nameCheck tests

--- test/features/checkUserData.test.js
+++ test/features/checkUserData.test.js
@@ -3,31 +3,32 @@ const CreateUser = require('../../lib/createUser')
 
 describe('Check User Existence and Data', () => {
   let createUser = new CreateUser()
-  let checkUserData = new CheckUserData
+  let checkUserData
   let name = 'ben'
   let password = 'any old thing'
   createUser.saveName(name)
   createUser.savePassword(password)
   createUser.create()
 
+  beforeEach(() => {
+    checkUserData = new CheckUserData
+  })
+
   describe('Check user exists', () => {
-    it('should return true if the user exists', done => {
-      console.log('BEGIN MY TEST')
-      let resultCHECK = (() => checkUserData.nameCheck(name))()
-      console.log('EXECUTE MY FUNCTION', resultCHECK)
+    it('should return true if the user exists', async (done) => {
+      let resultCHECK = await checkUserData.nameCheck(name)
       setTimeout(() => {
         expect(resultCHECK).toBe(true)
         done()
-      }, 4000)
-      console.log('INSIDE THE TIMEOUT', resultCHECK)
+      }, 100)
     })
-    xit('should return false if the user does not exist', done => {
-      let result = (() => checkUserData.nameCheck('BOB'))()
+
+    it('should return false if the user does not exist', async (done) => {
+      let result = await checkUserData.nameCheck('BOB')
       setTimeout(() => {
-        console.log('I AM THE RESULT', result)
         expect(result).toEqual(false)
         done()
-      }, 1000)
+      }, 100)
     })
   })
 })

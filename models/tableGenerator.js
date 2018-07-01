ddda19eb8345de3496e6673b90b21e452552e1b5
benfurber <ben@furber-consultancy.com>
2018-07-01 17:00:39 +0100

    Correct tableGenerator commands so tables can be built

--- models/tableGenerator.js
+++ models/tableGenerator.js
@@ -5,7 +5,8 @@ const connectionString = process.env.DATABASE_URL || env['development']
 const client = new pg.Client(connectionString)
 client.connect()
 const query = client.query(
-  'CREATE TABLE user(id SERIAL PRIMARY KEY, name VARCHAR(20), password VARCHAR(60))')
+  'CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(20), password VARCHAR(60));'
+)
 query.on('end', () => { client.end() })
 
 const connectionStringTest = process.env.DATABASE_URL || env['test']
@@ -13,5 +14,6 @@ const connectionStringTest = process.env.DATABASE_URL || env['test']
 const clientTest = new pg.Client(connectionStringTest)
 clientTest.connect()
 const queryTest = clientTest.query(
-  'CREATE TABLE user(id SERIAL PRIMARY KEY, name VARCHAR(20), password VARCHAR(60))')
+  'CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(20), password VARCHAR(60));'
+)
 queryTest.on('end', () => { clientTest.end() })

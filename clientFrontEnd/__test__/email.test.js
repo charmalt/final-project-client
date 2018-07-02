d1d67919e611f471a5e2e9f3a60970269e6d7664
John Newman <newmanj903@gmail.com>
2018-07-02 12:33:45 +0100

    Test for email component

---
+++ clientFrontEnd/__test__/email.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import Email from '../src/email'
+
+Enzyme.configure({adapter: new Adapter()})
+
+describe('<Email/>', () => {
+  let email
+  let obj
+  beforeEach(() => {
+    obj = {
+      id: 2,
+      mailto: 'igor@igor.com',
+      mailfrom: 'pino@chio.com',
+      mailbody: 'Am I a real boy?'
+    }
+    email = shallow(<Email key={1} cname={1} email={obj} />)
+  })
+
+  it('Displays mailto', () => {
+    expect(email.text()).toContain('Mail To: igor@igor.com')
+  })
+
+  it('Displays mailfrom', () => {
+    expect(email.text()).toContain('Mail From: pino@chio.com')
+  })
+
+  it('Displays mailbody', () => {
+    expect(email.text()).toContain('Message: Am I a real boy?')
+  })
+})
--- clientFrontEnd/src/email.js
+++ clientFrontEnd/src/email.js
@@ -4,9 +4,9 @@ class Email extends Component {
   render () {
     return (
       <div className={"Email" + this.props.cname}>
-        <label for="mailto">Mail To: </label> {this.props.email.mailto}<br/>
-        <label for="mailfrom">Mail From: </label> {this.props.email.mailfrom}<br/>
-        <label for="mailbody">Message: </label> {this.props.email.mailbody}
+        <label for="mailto">Mail To: </label>{this.props.email.mailto}<br/>
+        <label for="mailfrom">Mail From: </label>{this.props.email.mailfrom}<br/>
+        <label for="mailbody">Message: </label>{this.props.email.mailbody}
       </div>
     )
   }

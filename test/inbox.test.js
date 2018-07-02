1ef51b7a55d09e4a744d8b952031dca03ef6a18d
George Sykes <georgesykes86@gmail.com>
2018-07-02 21:37:31 +0100

    Add Inbox with addMessages method

---
+++ test/inbox.test.js
@@ -0,0 +1,23 @@
+const Inbox = require('../lib/inbox')
+
+describe('Inbox', () => {
+  let inbox
+  const messagesString = '[{"id": 1, "mailfrom": "George", "mailto": "Charlene", "mailbody": "HI"},' +
+    '{"id": 2, "mailfrom": "John", "mailto": "Igor", "mailbody": "BYE"}]'
+  const messages = [{"id": 1, "mailfrom": "George", "mailto": "Charlene", "mailbody": "HI"},
+    {"id": 2, "mailfrom": "John", "mailto": "Igor", "mailbody": "BYE"}]
+  const moreMessages = '[{"id": 1, "mailfrom": "George", "mailto": "Charlene", "mailbody": "HI"},' +
+    '{"id": 2, "mailfrom": "John", "mailto": "Igor", "mailbody": "BYE"},' +
+    '{"id": 3, "mailfrom": "Tom", "mailto": "Will", "mailbody": "Cheerio "}]'
+
+  beforeEach(() => {
+    inbox = new Inbox()
+  })
+
+  describe('addMessages', () => {
+    it('adds messages to the inbox', () => {
+      inbox.addMessages(messagesString)
+      expect(inbox.messages).toEqual(messages)
+    })
+  })
+})

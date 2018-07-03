c24e935175237829b491a338a8bd129bc8d7c14c
George Sykes <georgesykes86@gmail.com>
2018-07-03 07:11:27 +0100

    Add feature test for client connection

--- test/features/clientConnection.test.js
+++ test/features/clientConnection.test.js
@@ -1,30 +1,36 @@
-// const MailClient = require('../../lib/mailClient')
-// const net = require('net')
-//
-// describe('Feature Test', () => {
-//   let client
-//   let server
-//
-//   beforeAll(async () => {
-//     server = await net.createServer({ port: 1337, host: '127.0.0.1' }, () => {
-//       client = new MailClient(1337, '127.0.0.1', 5001, '127.0.0.1')
-//     })
-//     console.log = jest.fn()
-//   })
-//
-//   beforeEach(() => {
-//     console.log.mockClear()
-//   })
-//
-//   afterAll(() => {
-//     server.close()
-//   })
-//
-//   it('connects to pop Server', async (done) => {
-//     await client.send('message')
-//     setTimeout(() => {
-//       expect(console.log.mock.calls[0][0]).toEqual('Connected to server')
-//       done()
-//     }, 100)
-//   })
-// })
+const MailClient = require('../../lib/mailClient')
+const net = require('net')
+
+describe('Feature Test', () => {
+  let client
+  let server
+
+  beforeAll(async (done) => {
+    server = net.createServer()
+    server.listen({
+      host: 'localhost',
+      port: 1337
+    }, () => {
+      client = new MailClient(1337, '127.0.0.1', 5001, '127.0.0.1')
+      done()
+    })
+  })
+
+  beforeEach(() => {
+    console.log = jest.fn()
+  })
+
+  afterAll((done) => {
+    server.close(() => {
+      done()
+    })
+  })
+
+  it('connects to pop Server', (done) => {
+    client.send('message')
+    setTimeout(() => {
+      expect(console.log.mock.calls[0][0]).toEqual('Connected to server')
+      done()
+    }, 100)
+  })
+})

77378201d725feff1fa72387cd64709cbd8d3db9
John Newman <newmanj903@gmail.com>
2018-07-04 22:12:43 +0100

    Express logic put back in

--- app.js
+++ app.js
@@ -17,13 +17,16 @@ app.use(bodyParser.urlencoded({
 
 app.post('/login', (req, res) => {
   let user = req.body.sessionUser
+  mailClient = new MailClient(Env.smtpPort, Env.smtpHost, Env.popPort, Env.popHost, user)
 })
 
 app.get('/api/emails', (req, res) => {
-  res.json([{})
+  mailClient.receive()
+  res.json(mailClient.inbox.getMessages())
 })
 
 app.post('/api/emails', (req, res) => {
+  mailClient.send(req.body)
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
